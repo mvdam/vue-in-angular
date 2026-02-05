2335f89c75ec11f2d8975f8ed6d2ecdadcb02c06
Martin van Dam <martinvandam@protonmail.com>
2026-02-05 11:38:06 +0100

    basic vue in angular as webcomponent

--- angular-app/src/app/pages/vue/vue.component.ts
+++ angular-app/src/app/pages/vue/vue.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 
 @Component({
   selector: 'app-vue',
   templateUrl: './vue.component.html',
   styleUrl: './vue.component.scss',
+  imports: [CommonModule],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA, // Added for custom elements support
+  ],
 })
-export class VueComponent {}
+export class VueComponent {
+  public objectIds = signal<string[]>([]);
+  public blob = signal<Blob | null>(null);
+
+  public addObjectId() {
+    const uuid = crypto.randomUUID();
+    this.objectIds.update((current) => [...current, uuid]);
+  }
+
+  public onPublishFile(event: any) {
+    const receivedBlob = event.detail;
+    this.blob.set(receivedBlob);
+  }
+}
